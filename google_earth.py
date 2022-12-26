267ec0bc1fa9c1581d157e8a5358c8a819ed656c
Lucas-FC <54951174+Lucas-FC@users.noreply.github.com>
2022-12-26 18:12:33 +0100

    Update google_earth.py

--- google_earth.py
+++ google_earth.py
@@ -6,18 +6,20 @@ monkml = simplekml.Kml()
 
 book = xlrd.open_workbook("24_11_22.xls")
 sh = book.sheet_by_index(0)
-row = sh.nrows
+row = int(sh.nrows)
 
 print(row)
+
+nom_liaison = []
+siteB = []
+siteA = []
+latitudeB = []
+longitudeB = []
+latitudeA = []
+longitudeA = []
         
 for i in range(0, row):
-        nom_liaison = []
-        siteB = []
-        siteA = []
-        latitudeB = []
-        longitudeB = []
-        latitudeA = []
-        longitudeA = []
+
         nom_liaison.append(sh.cell_value(rowx=i, colx=0))
         siteB.append(sh.cell_value(rowx=i, colx=1))
         siteA.append(sh.cell_value(rowx=i, colx=6))        
@@ -25,12 +27,18 @@ for i in range(0, row):
         longitudeB.append(sh.cell_value(rowx=i, colx=3))        
         latitudeA.append(sh.cell_value(rowx=i, colx=7))       
         longitudeA.append(sh.cell_value(rowx=i, colx=8))
-        project_name = (siteA[i])
+        print (siteA)
+
+
+for x in range(0, row):
+
+        project_name = (nom_liaison[x])
         monkml.document.name = project_name
-        monkml.newpoint(name=siteB[i], coords=[(longitudeB[i],latitudeB[i])])
-        monkml.newpoint(name=siteA[i], coords=[(longitudeA[i],latitudeA[i])])
+        monkml.newpoint(name=siteB[x], coords=[(longitudeB[x],latitudeB[x])])
+        monkml.newpoint(name=siteA[x], coords=[(longitudeA[x],latitudeA[x])])
         name = str(project_name) + ".kml"
         monkml.save(name)
 
+print(longitudeB)
       
 
