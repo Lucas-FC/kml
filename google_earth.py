e8027f4792e9f3e12add1b39dd1d1f873de553cd
Lucas-FC <54951174+Lucas-FC@users.noreply.github.com>
2022-12-29 14:29:53 +0100

    Update google_earth.py

--- google_earth.py
+++ google_earth.py
@@ -1,14 +1,9 @@
 import xlrd
 import simplekml
 
-# initiation 
-monkml = simplekml.Kml()
-
-book = xlrd.open_workbook("24_11_22.xls")
+book = xlrd.open_workbook("input.xls")
 sh = book.sheet_by_index(0)
 row = int(sh.nrows)
-
-print(row)
 
 nom_liaison = []
 siteB = []
@@ -17,9 +12,13 @@ latitudeB = []
 longitudeB = []
 latitudeA = []
 longitudeA = []
+azimutA = []
+azimutB = []
+antenneA = []
+antenneB = []
+hmasiteA = []
         
 for i in range(0, row):
-
         nom_liaison.append(sh.cell_value(rowx=i, colx=0))
         siteB.append(sh.cell_value(rowx=i, colx=1))
         siteA.append(sh.cell_value(rowx=i, colx=6))        
@@ -27,18 +26,31 @@ for i in range(0, row):
         longitudeB.append(sh.cell_value(rowx=i, colx=3))        
         latitudeA.append(sh.cell_value(rowx=i, colx=7))       
         longitudeA.append(sh.cell_value(rowx=i, colx=8))
-        print (siteA)
+        azimutA.append(sh.cell_value(rowx=i, colx=10))
+        azimutB.append(sh.cell_value(rowx=i, colx=4))
+        antenneA.append(sh.cell_value(rowx=i, colx=11))
+        antenneB.append(sh.cell_value(rowx=i, colx=5))
+        hmasiteA.append(sh.cell_value(rowx=i, colx=9))
 
+for x in range(1, row):
 
-for x in range(0, row):
-
+        #SiteB
+        monkml = simplekml.Kml()
         project_name = (nom_liaison[x])
         monkml.document.name = project_name
-        monkml.newpoint(name=siteB[x], coords=[(longitudeB[x],latitudeB[x])])
-        monkml.newpoint(name=siteA[x], coords=[(longitudeA[x],latitudeA[x])])
-        name = str(project_name) + ".kml"
+        descriptionB = "Azimuth Site B : " + str(azimutB[x]) + "<br>Type d'antenne B : " + str(antenneB[x])
+        point1 = monkml.newpoint(name=siteB[x], coords=[(longitudeB[x],latitudeB[x])])
+        point1.description = (str(descriptionB))
+        point1.style.iconstyle.icon.href = 'https://cdn-icons-png.flaticon.com/512/9020/9020807.png'
+        
+        #SiteA
+        descriptionA = "Azimuth Site A : " + str(azimutA[x]) + "<br>Type d'antenne A : " + str(antenneA[x]) + "<br>HMA antenne : " + str(hmasiteA[x])
+        point2 = monkml.newpoint(name=siteA[x], coords=[(longitudeA[x],latitudeA[x])])
+        point2.description = (str(descriptionA))
+        point2.style.iconstyle.icon.href = 'https://cdn-icons-png.flaticon.com/512/9020/9020807.png'
+        
+        #Ligne
+        line = monkml.newlinestring(name=nom_liaison[x], coords=[(longitudeB[x], latitudeB[x]), (longitudeA[x], latitudeA[x])])
+        line.linestyle.color = simplekml.Color.red
+        name = str(x) + ".kml"
         monkml.save(name)
-
-print(longitudeB)
-      
-
